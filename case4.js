c1ed478b4af89b326e4be68de73ed000ea489507
Otto <22otto.wiman@glimnet.se>
2023-02-13 10:58:59 +0100

    har gjort texten mer lätt läst och tagit bort lite saker som inte används

--- case4.js
+++ case4.js
@@ -2,59 +2,37 @@ let url = "https://images-api.nasa.gov/search?q=";
 
 const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
+const search = document.querySelector("#search");
 
-  const search = document.querySelector("#search");
 
-  console.log(search.value);
+     button.addEventListener("click", () => {
+     url = `https://images-api.nasa.gov/search?q=${search.value}`;
 
-  url = `https://images-api.nasa.gov/search?q=${search.value}`;
-
-  console.log(url);
 
   fetch(url)
     .then((Response) => Response.json())
     .then((data) => {
-
-      console.log("data", data.collection.items[0]);
-
-      let items = data.collection.items;
-      items.forEach((item) => {
-        
-        // console.log(item.data[0].description)
-      console.log(item);
-      // content.innerHTML += item.links[0].href
-
-      // document.body.innerHTML += item.href
-
-      if (Array.isArray(item.links)){
-
+     let items = data.collection.items;
+    
+        items.forEach((item) => {
+      
+        console.log(item);
+      
+        if (Array.isArray(item.links)){
+      
         let img = new Image();
-        img.src = item.links[0].href;
-        // console.log(item.data[0].description)
-
         let des =  document.createElement("p")
+
+    
+        img.src = item.links[0].href;
         des.textContent = item.data[0].description
 
         document.body.appendChild(img);
         document.body.appendChild(des)
       }
-     
       });
-      //   document.body.innerHTML = data.collection.items.join()
      
-
-      // content.innerHTML = data.collection.items[0].links[0].href;
-      // content.innerHTML = data.collection.items[0].data[0].description;
-
 
 
     });
 });
-
-
-
-// göra en filtering tex datum, samt desribtion
-
-// console.log(item.links[0].href);
-
