6f805a46e9c1a498bf15a4d3f1b98947cbb3882b
Otto <22otto.wiman@glimnet.se>
2023-03-01 18:34:09 +0100

    har fixat att man kan filtera med årtal samt fixat css och tagit bort lite kod

--- case4.js
+++ case4.js
@@ -1,59 +1,40 @@
 let url = "https://images-api.nasa.gov/search?q=";
 const button = document.querySelector("button");
 const search = document.querySelector("#search");
+const content = document.querySelector("#content");
+const year = document.querySelector("#year");
 
 button.addEventListener("click", () => {
 url = `https://images-api.nasa.gov/search?q=${search.value}`;
 
   document.getElementById("content").innerHTML = "Laddar";
 
-  fetch(url)
+  fetch(url + "&year_end=" + year.value)
     .then((Response) => Response.json())
     .then((data) => {
+
       document.getElementById("content").innerHTML = "";
+      // detta gör att det står bara ladda när den laddar, raden 
+
       let items = data.collection.items;
 
-      async function getData() {
-        let response = await fetch("https://images-api.nasa.gov/search?q=nasa");
-        // awit betyder typ ta det lungt så länge, datan är påväg
-        let data = await response.json();
-        // let data = awit response.json() skapar en variabel som omvandlar, spåket från json till javascript
-
-        console.log(data.collection.items);
-
-        let filteredArray = data.collection.items.filter(
-          (item) =>
-            new Date(item.data[0].date_created) >
-            new Date("2010-05-14T00:00:00Z")
-          // raden ovanför bestämer när den ska starta tex 2010 då tar den bara efter det datumet,
-        );
-
-        if (
-          prompt(
-            "Vill du logga ut alla items efter 2010? Skriv ja eller nej"
-          ) === "ja"
-        ) {
-          console.log(data.collection.items);
-  
-        } else {
-          console.log(filteredArray);
-        }
-      }
-
-      getData();
-
+     
       items.forEach((item) => {
         console.log(item);
-
         if (Array.isArray(item.links)) {
+
           let img = new Image();
           let des = document.createElement("p");
+          // här skapar jag variablerna
 
           img.src = item.links[0].href;
           des.textContent = item.data[0].description;
+          // här så blir variablen lika med api i dokumentet 
+
 
           document.getElementById("content").appendChild(img);
           document.getElementById("content").appendChild(des);
+          // här bestämer man vart det ska i dokumentet
         }
       });
     });
